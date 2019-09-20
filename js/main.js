93c93a9e9599b1e7a23f9f4828a59eb1123179b6
Ilya <brikbazuko@yandex.ru>
2019-09-20 16:33:43 +0300

    arrow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    nav:true,
+    navText: ["<img src='img/left-icon-green.png'>","<img src='img/right-icon-green.png'>"]
+  });
 });
